757765059a42430ce2d1d6b540aa7fa9293c9450
= <=>
2026-07-30 16:52:17 -0600

    change import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import App from './pages/App.jsx'
 import Operaciones from './pages/Operaciones.jsx'
@@ -8,13 +8,13 @@ import AcercaDe from './pages/AcercaDe.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Operaciones />} />
           <Route path="acerca-de" element={<AcercaDe />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
